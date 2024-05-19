52dd0d96d8f08b1f1949f3e487a537d1eb964d7f
Princewill Iroka <princewilliroka@outlook.com>
2024-05-20 00:07:38 +0100

    Update AuthHelper

--- src/utils/helpers/AuthHelper.js
+++ src/utils/helpers/AuthHelper.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import config from "../config";
+import config from "../../config";
 
 const SaltRounds = 8;
 const JWT_SECRET = config.jwtSecret;
